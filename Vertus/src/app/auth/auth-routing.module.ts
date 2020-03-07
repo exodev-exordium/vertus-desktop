a8380ec2e485ee96bb3000607244316db0ddbb93
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-03-07 09:14:05 -0500

    Authentication Handler (most likely optional)

---
+++ Vertus/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthPage } from './auth.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthPage
+  },
+  {
+    path: 'signin',
+    loadChildren: () => import('./signin/signin.module').then( m => m.SigninPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthPageRoutingModule {}
