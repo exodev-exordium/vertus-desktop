efda5ab9fcad3d1c6a6d2130cb9919de1f890e82
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-03-07 09:01:27 -0500

    Remove folder routing, add and change to pages routing.

--- Vertus/src/app/app-routing.module.ts
+++ Vertus/src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'pages/dashboard',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'pages',
+    loadChildren: () => import('./pages/pages.module').then( m => m.PagesPageModule)
   }
 ];
 
